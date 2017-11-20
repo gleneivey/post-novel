b08183cfef6b81a47849199bd7198ee4a0ebc67c
Glen E. Ivey <gei@mcn.org>
2017-11-20 07:50:05 -0800

    23,856 words. Wakey wakey

--- docs/assets/js/footnotes.js
+++ docs/assets/js/footnotes.js
@@ -13,5 +13,6 @@ function punctToNum(punctString) {
 }
 
 var footnoteContent = {
-  "!": "A \"nibling\" is the child of a person's sibling."
+  "!": "A \"nibling\" is the child of a person's sibling.",
+  "@": "Mas is the second-youngest of eir four children, introduced in one of the middle chapters of Part 1 that isn't written yet."
 };
